807faf628378eb199ba8806ded6b91b212c86744
oinkcat <softcat@live.ru>
2020-10-16 13:58:34 +0300

    Исправление идентификатора тегов

--- ReactHH/ClientApp/src/components/Tags.jsx
+++ ReactHH/ClientApp/src/components/Tags.jsx
@@ -3,7 +3,7 @@
 export function Tags(props) {
     return (
         <ul className="tag-list">
-            {props.list.map(tag => <li key={tag}>{tag}</li>)}
+            {props.list.map((tag, i) => <li key={i}>{tag}</li>)}
         </ul>
     );
 }
